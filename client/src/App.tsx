b86f3dc7b5db96b878e54780a4601fe68d01225a
unknown <subashchandraboseravi45@gmail.com>
2025-05-29 14:31:55 +0530

    added not found

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './components/dashboard/Home';
 import AcceptInvite from './components/AcceptInvite';
-
+import LandingPage from './components/LandingPage';
+import NotFound from './components/NotFound';
 function App() {
-
+  
   return (
     <>
       <div>
         <Router>
           <Routes>
+            <Route path="/" element={<LandingPage />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/accept-invite" element={<AcceptInvite />} />
             <Route element={<ProtectedRoute />}>
               <Route path="/dashboard" element={<Home />} />
             </Route>
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Router>
       </div>
